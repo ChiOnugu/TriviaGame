505a48f95b68f2cd4c21a8504fc90ff8bc943104
Chinedu Onugu <teddybluez@users.noreply.github.com>
2017-08-04 00:45:35 -0400

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,8 @@
 
 
    $(document).ready(function() {
+
+
 // Create a function that creates the start button and initial screen
 
 
@@ -14,10 +16,9 @@
 
     initialScreen();
 
-//Create a function, generateHTML(), that is triggered by the start button, and generates the HTML seen on the project video...
 
     $("body").on("click", ".start-button", function(event){
-	event.preventDefault();  // added line to test issue on GitHub Viewer
+	event.preventDefault();  
 	clickSound.play();
 	generateHTML();
 
@@ -28,17 +29,23 @@
 }); // Closes start-button click
 
     $("body").on("click", ".answer", function(event){
+	
 	//answeredQuestion = true;
+	
+
 	clickSound.play();
 	selectedAnswer = $(this).text();
 	if(selectedAnswer === correctAnswers[questionCounter]) {
+		
 		//alert("correct");
 
 		clearInterval(theClock);
 		generateWin();
 	}
 	else {
+		
 		//alert("wrong answer!");
+		
 		clearInterval(theClock);
 		generateLoss();
 	}
@@ -49,40 +56,41 @@
     $("body").on("click", ".reset-button", function(event){
 	clickSound.play();
 	resetGame();
-}); // Closes reset-button click
+}); 
+     // Closes reset-button click
 
 
 
-});  //  Closes jQuery wrapper
+});  
 
     function generateLossDueToTimeOut() {
 	unansweredTally++;
 	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/x.png'>";
 	$(".mainArea").html(gameHTML);
-	setTimeout(wait, 1000);  //  change to 4000 or other amount
+	setTimeout(wait, 1000);  
 }
 
     function generateWin() {
 	correctTally++;
 	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
 	$(".mainArea").html(gameHTML);
-	setTimeout(wait, 1000);  //  change to 4000 or other amount
+	setTimeout(wait, 1000);  
 }
 
     function generateLoss() {
 	incorrectTally++;
 	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/x.png'>";
 	$(".mainArea").html(gameHTML);
-	setTimeout(wait, 1000); //  change to 4000 or other amount
+	setTimeout(wait, 1000); 
 }
 
     function generateHTML() {
-	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>20</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
 	$(".mainArea").html(gameHTML);
 }
 
     function wait() {
-	if (questionCounter < 7) {
+	if (questionCounter < 8) {
 	questionCounter++;
 	generateHTML();
 	counter = 20;
@@ -91,7 +99,7 @@
 	else {
 		finalScreen();
 	}
-
+   
 
 }
 
@@ -128,10 +136,10 @@
 var startScreen;
 var gameHTML;
 var counter = 20;
-var questionArray = ["What is the capital of Australia?", "What is the capital of Liberia?", "What is the capital of Taiwan?", "What is the capital of Japan?", "What is the capital of China?", "What is the capital of Turkey?", "What is the capital of Colombia?", "What is the capital of India?"];
-var answerArray = [["Canberra", "Melbourne", "Sydney", "Darwin"], ["Arthington","Monrovia","Tuzon","Marshall"], ["Tainan City", "Taichung", "Taipei", "Hsinchu"], ["Kyoto","Hiroshima","Tokyo","Osaka"], ["Hong Kong", "Macau", "Shanghai", "Beijing"], ["Ankara","Istanbul","Antalya","Bursa"], ["Medellin", "Bogota", "Cartagena", "Cali"], ["Mumbai","Hyderabad","Bangalore","New Delhi"]];
-var imageArray = ["<img class='center-block img-right' src='assets/images/australia.png'>", "<img class='center-block img-right' src='assets/images/liberia.png'>", "<img class='center-block img-right' src='assets/images/taiwan.png'>", "<img class='center-block img-right' src='assets/images/japan.png'>", "<img class='center-block img-right' src='assets/images/china.png'>", "<img class='center-block img-right' src='assets/images/turkey.png'>", "<img class='center-block img-right' src='assets/images/colombia.png'>", "<img class='center-block img-right' src='assets/images/india.png'>"];
-var correctAnswers = ["A. Canberra", "B. Monrovia", "C. Taipei", "C. Tokyo", "D. Beijing", "A. Ankara", "B. Bogota", "D. New Delhi"];
+var questionArray = ["What is the capital of Nigeria?", "What is the capital of Australia?", "What is the capital of Liberia?", "What is the capital of Taiwan?", "What is the capital of Japan?", "What is the capital of China?", "What is the capital of Turkey?", "What is the capital of Colombia?", "What is the capital of India?"];
+var answerArray = [["Lagos", "Enugu", "Portharcourt", "Abuja"], ["Canberra", "Melbourne", "Sydney", "Darwin"], ["Arthington","Monrovia","Tuzon","Marshall"], ["Tainan City", "Taichung", "Taipei", "Hsinchu"], ["Kyoto","Hiroshima","Tokyo","Osaka"], ["Hong Kong", "Macau", "Shanghai", "Beijing"], ["Ankara","Istanbul","Antalya","Bursa"], ["Medellin", "Bogota", "Cartagena", "Cali"], ["Mumbai","Hyderabad","Bangalore","New Delhi"]];
+var imageArray = ["<img class='center-block img-right' src='assets/images/Nigeria.png'>", "<img class='center-block img-right' src='assets/images/australia.png'>", "<img class='center-block img-right' src='assets/images/liberia.png'>", "<img class='center-block img-right' src='assets/images/taiwan.png'>", "<img class='center-block img-right' src='assets/images/japan.png'>", "<img class='center-block img-right' src='assets/images/china.png'>", "<img class='center-block img-right' src='assets/images/turkey.png'>", "<img class='center-block img-right' src='assets/images/colombia.png'>", "<img class='center-block img-right' src='assets/images/india.png'>"];
+var correctAnswers = ["D. Abuja", "A. Canberra", "B. Monrovia", "C. Taipei", "C. Tokyo", "D. Beijing", "A. Ankara", "B. Bogota", "D. New Delhi"];
 var questionCounter = 0;
 var selecterAnswer;
 var theClock;
